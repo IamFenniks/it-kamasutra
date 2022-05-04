ab8db5ba72b207cd277656dcf4f339834000ec09
IamFennics <andrej.darmenko@gmail.com>
2022-05-04 20:41:38 +0300

    Import / Export

---
+++ src/Tecknologies.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Tecknologies = () => {
+    return (
+      <div>
+        <ul>
+          <li>Html</li>
+          <li>CSS</li>
+          <li>JS</li>
+          <li>JSX </li>
+        </ul>
+      </div>  
+    );
+  }
+
+  export default Tecknologies;
