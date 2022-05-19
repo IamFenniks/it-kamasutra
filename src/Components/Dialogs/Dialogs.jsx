bd904db496f57c02d23d99302e991047c8c77864
IamFennics <andrej.darmenko@gmail.com>
2022-05-19 20:50:56 +0300

    mapping of Dialogs and Posts

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -2,28 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from './Dialogs.module.css';
 
-// BLL Start
-    let friendsData = [
-        { id: '0', name: 'Лариса'  },
-        { id: '1', name: 'Кристина'},
-        { id: '2', name: 'Коля'    },
-        { id: '3', name: 'Карина'  },
-        { id: '4', name: 'Витя'    },
-        { id: '5', name: 'Лена'    },
-        { id: '6', name: 'Никита'  }
-    ];
-
-    let messageData = [
-        { id: '0', message: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. То подпоясал великий над толку мир своего взобравшись собрал возвращайся запятой.'  },
-        { id: '1', message: 'Далеко-далеко за словесными горами в стране гласных и согласных, живут рыбные тексты. Наш даже точках но?'},
-        { id: '2', message: 'Далеко-далеко за, словесными горами в стране гласных и согласных живут рыбные тексты. Всеми безорфографичный рукопись речью встретил коварных назад. Возвращайся оксмокс продолжил себя коварных это, всеми пустился парадигматическая несколько языкового эта, рекламных наш строчка?'    },
-        { id: '3', message: 'Привет!'  },
-        { id: '4', message: 'Привет!'    },
-        { id: '5', message: 'Привет!'    },
-        { id: '6', message: 'Привет!'  }
-    ];
-// BLL Finish
-
 // Inside Comps Start
     const Friend = (props) => {
         return (
@@ -43,6 +21,33 @@ import style from './Dialogs.module.css';
 // Inside Comps Finish
 
 const Dialogs = () => {
+    // BLL Start
+        let friendsData = [
+            { id: '0', name: 'Лариса'  },
+            { id: '1', name: 'Кристина'},
+            { id: '2', name: 'Коля'    },
+            { id: '3', name: 'Карина'  },
+            { id: '4', name: 'Витя'    },
+            { id: '5', name: 'Лена'    },
+            { id: '6', name: 'Никита'  }
+        ];
+
+        let messageData = [
+            { id: '0', message: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. То подпоясал великий над толку мир своего взобравшись собрал возвращайся запятой.'  },
+            { id: '1', message: 'Далеко-далеко за словесными горами в стране гласных и согласных, живут рыбные тексты. Наш даже точках но?'},
+            { id: '2', message: 'Далеко-далеко за, словесными горами в стране гласных и согласных живут рыбные тексты. Всеми безорфографичный рукопись речью встретил коварных назад. Возвращайся оксмокс продолжил себя коварных это, всеми пустился парадигматическая несколько языкового эта, рекламных наш строчка?'    },
+            { id: '3', message: 'Привет!' },
+            { id: '4', message: 'Привет!' },
+            { id: '5', message: 'Привет!' },
+            { id: '6', message: 'Привет!' }
+        ];
+    // BLL Finish
+
+    // .Maping start
+        let elementFriend = friendsData.map(f => <Friend name={ f.name } id={ f.id } />);
+        let elementMessage = messageData.map(m => <Item message={ m.message } id={ m.id } />);
+    // .Maping finish
+
     return (
         <main className={style.dialogs}>
             <h3>Диалоги</h3>
@@ -50,22 +55,12 @@ const Dialogs = () => {
             <div className={style.my_friends}>
 
                 <nav className={style.nav}>
-                    <Friend name={ friendsData[0].name }   id={ friendsData[0].id } />
-                    <Friend name={ friendsData[1].name }   id={ friendsData[1].id } />
-                    <Friend name={ friendsData[2].name }   id={ friendsData[2].id } />
-                    <Friend name={ friendsData[3].name }   id={ friendsData[3].id } />
-                    <Friend name={ friendsData[4].name }   id={ friendsData[4].id } />
-                    <Friend name={ friendsData[5].name }   id={ friendsData[5].id } />
-                    <Friend name={ friendsData[6].name }   id={ friendsData[6].id } />
+                    { elementFriend }
                 </nav>
             </div>
 
             <div className={style.messages}>
-                <Item message={ messageData[0].message } id={ messageData[0].id } />
-                <Item message={ messageData[1].message } id={ messageData[1].id } />
-                <Item message={ messageData[2].message } id={ messageData[2].id } />
-                <Item message={ messageData[3].message } id={ messageData[3].id } />
-                <Item message={ messageData[4].message } id={ messageData[4].id } />
+                { elementMessage }
             </div>
         </main>
     );
--- src/Components/Profile/MyPosts.jsx
+++ src/Components/Profile/MyPosts.jsx
@@ -2,27 +2,32 @@ import React from "react";
 import style from './MyPosts.module.css';
 
 const MyPosts = () => {
+    const Post = (props) => {
+        return (
+            <div className={style.post}>
+                <div className={style.ava}>{ props.id }</div>
+                <div className={style.post_text}>
+                    <p>{ props.postText }</p>
+                </div>
+            </div>
+        );
+    }
+
+    // BLL Start
+    let postData = [
+        { id: 0, text: 'Это статья номер 1' },
+        { id: 1, text: 'Это статья номер 2' },
+        { id: 2, text: 'Это статья номер 3' }
+    ];
+
+    let postElement = postData.map( p => <Post postText={p.text} id={p.id} />);
+    // BLL Finish
+
     return (
         <div className={style.my_posts}>
             <h3>Мои статьи</h3>
-            <div className={style.post}>
-                <div className={style.ava}>1</div>
-                <div className={style.post_text}>
-                    <p>Это статья номер 1</p>
-                </div>
-            </div>
-            <div className={style.post}>
-                <div className={style.ava}>2</div>
-                <div className={style.post_text}>
-                    <p>Это статья номер 2</p>
-                </div>
-            </div>
-            <div className={style.post}>
-                <div className={style.ava}>3</div>
-                <div className={style.post_text}>
-                    <p>Это статья номер 3</p>
-                </div>
-            </div>
+
+            { postElement }
         </div>
     );
 }
