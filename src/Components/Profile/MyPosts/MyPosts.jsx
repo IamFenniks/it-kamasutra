a3089fb1d13fb0eb705667cf15584403de40ff20
IamFennics <andrej.darmenko@gmail.com>
2022-05-23 14:40:24 +0300

    Adding React.createRef() callback function.

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -5,11 +5,20 @@ import style from './MyPosts.module.css';
 const MyPosts = (props) => {
     
     let postElement = props.postData.map( (p, index) => <Post key={index} postText={p.text} id={p.id} />);
+    let newPostElement = React.createRef();
+    let addPost = () => { 
+        let postText = newPostElement.current.value;
+        alert(postText);
+    };
 
     return (
         <div className={style.my_posts}>
             <h3>Мои статьи</h3>
 
+            <div className={style.btn_group}>
+                <div><textarea ref={ newPostElement } placeholder="Текст..."></textarea></div>
+                <div><button type='submit' onClick={ addPost }>OK</button></div>
+            </div>
             { postElement }
         </div>
     );
