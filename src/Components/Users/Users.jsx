bd7f6937d1f66dbd9fca263bfafede72aa2497db
IamFennics <andrej.darmenko@gmail.com>
2022-07-10 15:22:06 +0300

    Installed 'axios'. User API in User.jsx

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,41 +1,22 @@
+import axios from "axios";
 import React from "react";
 import style from "./Users.module.css"
+import userPhoto from "./../../images/users.png"
 
 const Users = (props) => {
-        props.setUsers ([
-            { 
-                id: 0,
-                followed: true,
-                userImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOa31A3Sstz7vDPuZQ_Q0exSI9K6oKIVtig&usqp=CAU',
-                name: 'Андрей',
-                status: 'Команда',
-                location: { country: 'Россия' , city: 'Мариуполь' } 
-            },
-            { 
-                id: 1,
-                followed: false,
-                userImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZC5LyXIKd6CkjtmgegFMd5qpCPhy22gGWzA&usqp=CAU',
-                name: 'Иван',
-                status: 'Команда',
-                location: { country: 'Россия' , city: 'Мариуполь' } 
-            },
-            { 
-                id: 2,
-                followed: true,
-                userImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJKvGaF13ty4bRwZmOTGf4mkwBEIx85bLK9A&usqp=CAU',
-                name: 'Марья',
-                status: 'Команда',
-                location: { country: 'Россия' , city: 'Мариуполь' } 
-            },
-            { 
-                id: 3,
-                followed: true,
-                userImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJKvGaF13ty4bRwZmOTGf4mkwBEIx85bLK9A&usqp=CAU',
-                name: 'Марья',
-                status: 'Команда',
-                location: { country: 'Россия' , city: 'Мариуполь' } 
-            }
-        ])
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => { props.setUsers(response.data.items); })
+
+        // props.setUsers ([
+        //     { 
+        //         id: 0,
+        //         followed: true,
+        //         userImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyOa31A3Sstz7vDPuZQ_Q0exSI9K6oKIVtig&usqp=CAU',
+        //         name: 'Андрей',
+        //         status: 'Команда',
+        //         location: { country: 'Россия' , city: 'Мариуполь' } 
+        //     }
+        // ])
 
     return (
         <main className={style.users}>
@@ -44,7 +25,7 @@ const Users = (props) => {
             {
                 props.users.map(u => <div key={u.id} className={style.user_element}>
                     <div className={style.left_side}>
-                        <img src={u.userImg} alt="user avtar" />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto } alt="user avtar" />
                         { u.followed 
                             ? <button onClick={ () => { props.unfollowed(u.id) } }>Follow</button> 
                             : <button onClick={ () => { props.followed(u.id) } }>Unfollow</button>
@@ -54,12 +35,12 @@ const Users = (props) => {
 
                     <div className={style.rigth_side}>
                         <div>
-                            <p>Имя: {u.name}</p>
-                            <p>Статус: {u.status}</p>
+                            <p><b><i>Имя:</i></b> {u.name}</p>
+                            <p><b><i>Статус:</i></b> {u.status}</p>
                         </div>
                         <div>
-                            <p>Страна: {u.location.country}</p>
-                            <p>Город: {u.location.city}</p>
+                            <p><b><i>Страна:</i></b> {"u.location.country"}</p>
+                            <p><b><i>Город:</i></b> {"u.location.city"}</p>
                         </div>
                     </div>
                 </div>
